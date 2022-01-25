e11ff7916ff9d53714521325d3ef32b9fcd4a204
mina-han <77199528+mina-han@users.noreply.github.com>
2022-01-25 19:15:28 +0900

    인증서 무한 등록
    break 조건 필요

---
+++ truffle/test/generateRandomCode.js
@@ -0,0 +1,9 @@
+module.exports = {
+  addstr: function generateRandomCode(n) {
+    let str = "";
+    for (let i = 0; i < n; i++) {
+      str += Math.floor(Math.random() * 10);
+    }
+    return str;
+  },
+};
